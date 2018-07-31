356f9460b1f33587988e2e654f7bd5f49816a4b3
Musajan <manzil_11@hotmail.com>
2018-07-31 11:12:22 +0800

    annotation

--- src/main/java/com/akburkut/tv/cms/akburkut/annotation/Test.java
+++ src/main/java/com/akburkut/tv/cms/akburkut/annotation/Test.java
@@ -1,5 +1,9 @@
 package com.akburkut.tv.cms.akburkut.annotation;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * @Author: Sherlock
  * @Description:
@@ -30,9 +34,61 @@ public class Test {
 
     private static String query(Filter filter) {
         StringBuilder builder = new StringBuilder();
-        //1.获取到Class
+        // 1.获取到Class
         Class c = filter.getClass();
+        // 2.获取到table的名字
+        boolean exists = c.isAnnotationPresent(Table.class);
+        if (!exists) {
+            return null;
+        }
 
-        return null;
+        Table table = (Table) c.getAnnotation(Table.class);
+        String tableName = table.value();
+
+        builder.append("select * from ").append(tableName).append(" where 1 = 1");
+
+        // 3.便利所有的字段
+        Field[] fileds = c.getDeclaredFields();
+        for (Field field : fileds) {
+            // 4.处理每个字段对应的sql
+            // 4.1.拿到字段的名字
+            boolean fieldExists = field.isAnnotationPresent(Column.class);
+            if (!fieldExists) {
+                continue;
+            }
+            Column column = field.getAnnotation(Column.class);
+            String columnName = column.value();
+            // 4.2.拿到字段的值
+            String filedName = field.getName();
+            String getMethodName = "get" + filedName.substring(0, 1).toUpperCase()
+                    + filedName.substring(1);
+            Object fieldValue = null;
+            try {
+                Method getMathod = c.getMethod(getMethodName);
+                fieldValue = getMathod.invoke(filter);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            // 4.3.瓶装sql
+            if (fieldValue != null && fieldValue instanceof String) {
+                builder.append(" and ").append(filedName);
+                if (((String) fieldValue).contains(",")) {
+                    String[] values = ((String) fieldValue).split(",");
+                    builder.append(" in(");
+                    for (String v : values) {
+                        builder.append("'").append(v).append("'").append(",");
+                    }
+                    builder.deleteCharAt(builder.length() - 1);
+                    builder.append(")");
+                } else {
+                    builder.append(" = ").append("'").append(fieldValue).append("'");
+                }
+            } else if (fieldValue instanceof Integer && (Integer)fieldValue != 0) {
+                builder.append(" and ").append(filedName);
+                builder.append(" = ").append(fieldValue);
+            }
+        }
+
+        return builder.toString();
     }
 }
